0bbb42ba4b85207500974a58271b28784c0da8a5
ZiCode0 <zed.code.05@gmail.com>
2021-03-26 13:44:17 +1200

    :sparkles: add route to auth api :sparkles:

--- app.py
+++ app.py
@@ -14,6 +14,37 @@ def gate_status():
     return 'Status: online'
 
 
+@app.route('/api/auth', methods=['GET', 'POST'])
+def auth_request():
+    """
+    Thread login router
+    :return: auth result
+    """
+    content = request.json
+    queued_request = queue.Queue()
+    Thread(target=auth_thread_request, args=(content, queued_request)).start()
+    auth_answer = queued_request.get()
+    return auth_answer
+
+
+def auth_thread_request(content, queue_res):
+    """
+    Connect login microservice and get result in every thread
+    :param content:
+    :param queue_res:
+    :return:
+    """
+    content = json.dumps(content).encode('utf-8')
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('127.0.0.1', 2289))
+    client.send(content)
+    resp = client.recv(1024)
+    if resp:
+        queue_res.put(resp)
+    else:  # TODO: fill else if empty/None answer
+        print('empty')
+
+
 @app.route('/api/login', methods=['GET', 'POST'])
 def login_request():
     """

d99c4e36d6e8b3e77a6da693a48f2b0003979d4a
ZiCode0 <zed.code.05@gmail.com>
2021-03-28 21:35:40 +1200

    :white_check_mark: update auth test :white_check_mark:

--- test/test_auth_request.py
+++ test/test_auth_request.py
@@ -3,9 +3,26 @@ import requests
 
 token = sys.argv[1]
 
-auth_request = {'request': 'auth',
-                'body': token}
+auth_test = {'request': 'test'}
+auth_no_request_key = {'body': ''}
+auth_request_request_value_invalid = {'request': 'aaa', 'body': ''}
+auth_request_body_empty = {'request': 'auth', 'body': ''}
+auth_request_token_success = {'request': 'auth', 'body': token}
+auth_request_token_invalid = {'request': 'auth', 'body': token[:-1]}
 
-response = requests.get('http://127.0.0.1:5000/api/auth', json=auth_request)
-response = response.content.decode('utf-8')
-print(response)
+tests = [auth_test,
+         auth_no_request_key,
+         auth_request_body_empty,
+         auth_request_request_value_invalid,
+         auth_request_token_invalid,
+         auth_request_token_success]
+
+
+for test_request in tests:
+    try:
+        response = requests.get('http://127.0.0.1:5000/api/auth', json=test_request)
+        response = response.content.decode('utf-8')
+        print(test_request, response)
+        # print()
+    except Exception as ex:
+        print(ex)

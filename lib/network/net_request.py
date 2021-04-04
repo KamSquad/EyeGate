1f244fe381cc553419c0766c1d3b4567e215df35
ZiCode0 <zed.code.05@gmail.com>
2021-04-04 13:59:56 +1200

    :sparkles: add schemas check :sparkles:

--- lib/network/net_request.py
+++ lib/network/net_request.py
@@ -1,3 +1,5 @@
+from schema import SchemaError, Schema, And, Use
+
 answer_codes = {'success': 200,  # OK. Успешный запрос.
                                  # Ответ в виде успешного запроса.
                 'object_created': 201,  # Успешный запрос. Был создан объект.
@@ -13,3 +15,29 @@ def make_answer_json(answer_code, body):
     ans_json = {'code': answer_code,
                 'body': body}  # form answer as json
     return ans_json
+
+
+def check_schema(inp_conf_schema, dict_object):
+    try:
+        inp_conf_schema.validate(dict_object)
+        return True
+    except SchemaError:
+        return False
+
+
+save_token_request_schema = Schema({
+    'request': And(Use(str)),
+    'body': {
+        'auth': And(Use(str)),
+        'push': And(Use(str)),
+        'univer': And(Use(str)),
+        'fak': And(Use(str)),
+        'state': And(Use(str))
+    }
+})
+
+
+def check_token_save_request_struct(dict_obj):
+    check_res = check_schema(inp_conf_schema=save_token_request_schema,
+                             dict_object=dict_obj)
+    return check_res
